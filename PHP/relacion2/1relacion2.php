38848adefc4c7d425419149c792b4ed0f75ad61b
Francisco Aybar <frannur1.ir@gmail.com>
2025-10-07 21:27:46 +0200

    Algunos echos, pero aún faltan

--- PHP/relacion2/1relacion2.php
+++ PHP/relacion2/1relacion2.php
@@ -8,35 +8,46 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
 </head>
 <body>
-    <div class="container mt-3">
+    <div class="bg-info-subtle">
+        <div class="d-flex justify-content-center align-items-center vh-100">
+            <!-- Cuando el cálculo está en el pripio archiv
+             hay que llamarlo desde action (así mismo) -->
+            <form class="p-5 border border-warning rounded shadow bg-light" action="<?php echo $_SERVER['PHP_SELF'] ?>" method="post">
+                <h3 class="text-info-emphasis">Calculadora básica con 2 números y un operador</h3>
+                
+                <div class="mb-3">
+                    <label class="form-label" for="numero1">Introduce número 1: </label>
+                    <input class="form-control" type="number" step="0.01" min="0" placeholder="0,00" name="numero1" id="numero1">
+                </div>    
+                
+                <div class="mb-3">
+                    <label class="form-label" for="operador">Elige operador: </label>
+                    <select class="form-select" name="operador" id="operador">
+                        <option value="sumar">+</option>
+                        <option value="resta">-</option>
+                        <option value="producto">*</option>
+                        <option value="division">/</option>
+                        <option value="resto">%</option>
+                    </select>
+                </div>
         
-        <form class="w-50 m-auto p-3" action="" method="get">
-            <h3>Calculadora básica con 2 números y un operador</h3>
-            
-            <div class="mb-3">
-                <label class="form-label" for="numero1">Introduce número 1: </label>
-                <input class="form-control" type="text" name="numero1" id="numero1">
-            </div>    
-            
-            <div class="mb-3">
-                <label class="form-label" for="operador">Elige operador: </label>
-                <select class="form-select" name="operador" id="operador">
-                    <option value="sumar">+</option>
-                    <option value="resta">-</option>
-                    <option value="producto">*</option>
-                    <option value="division">/</option>
-                    <option value="resto">%</option>
-                </select>
-            </div>
+                <div class="mb-3">
+                    <label class="form-label" for="numero2">Introduce número 2: </label>
+                    <input class="form-control" type="number" step="0.01" placeholder="0,00" name="numero2" id="numero2">
+                </div>
+                
+                <input class="form-control" type="submit" value="Enviar">
+            </form>    
+        </div>
     
-            <div class="mb-3">
-                <label class="form-label" for="numero2">Introduce número 2: </label>
-                <input class="form-control" type="text" name="numero2" id="numero2">
-            </div>
-            
-            <input class="form-control" type="submit" value="Enviar">
-        </form>
+        <p>
+            <?php
+                echo "algo";
+            ?>
+        </p>
+    
     </div>
+    
 
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
 </body>
---
+++ PHP/relacion2/6relacion2.php
@@ -0,0 +1,95 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Relación 2 - Ejercicio 6</title>
+    <link rel="shortcut icon" href="./logo-ies-playamar.png" type="image/x-icon">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+</head>
+<body class="bg-info-subtle">
+
+    <div class="container">
+
+        <p class="fs-1 text-center mt-2 py-2 shadow rounded bg-light">Personal</p>
+        <div class="table-responsive">
+            <table class="table table-hover table-striped table border border-primary border-opacity-50">
+                <thead>
+                    <tr class="table-success">
+                        <th>#</th>
+                        <th>Nombre</th>
+                        <th>Primer Apellido</th>
+                        <th>Correo Electronico</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>1</td>
+                        <td>Lanny</td>
+                        <td>Joynt</td>
+                        <td>ljoynt0@phpbb.com</td>
+                    </tr>
+                    <tr>
+                        <td>2</td>
+                        <td>Lorna</td>
+                        <td>Williamson</td>
+                        <td>lwilliamson1@nymag.com</td>
+                    </tr>
+                    <tr>
+                        <td>3</td>
+                        <td>Ody</td>
+                        <td>Bindin</td>
+                        <td>obindin2@reverbnation.com</td>
+                    </tr>
+                    <tr>
+                        <td>4</td>
+                        <td>Natalina</td>
+                        <td>Allix</td>
+                        <td>nallix3@xrea.com</td>
+                    </tr>
+                    <tr>
+                        <td>5</td>
+                        <td>Kore</td>
+                        <td>Copozio</td>
+                        <td>kcopozio4@ehow.com</td>
+                    </tr>
+                    <tr>
+                        <td>6</td>
+                        <td>Glenn</td>
+                        <td>Leverington</td>
+                        <td>gleverington5@slate.com</td>
+                    </tr>
+                    <tr>
+                        <td>7</td>
+                        <td>Paolo</td>
+                        <td>Arsnell</td>
+                        <td>parsnell6@jalbum.net</td>
+                    </tr>
+                    <tr>
+                        <td>8</td>
+                        <td>Nessa</td>
+                        <td>Kitchenside</td>
+                        <td>nkitchenside7@apple.com</td>
+                    </tr>
+                    <tr>
+                        <td>9</td>
+                        <td>Ellswerth</td>
+                        <td>McBrearty</td>
+                        <td>emcbrearty8@studiopress.com</td>
+                    </tr>
+                    <tr>
+                        <td>10</td>
+                        <td>Rivy</td>
+                        <td>McKerrow</td>
+                        <td>rmckerrow9@list-manage.com</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+
+    </div>
+
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.min.js" integrity="sha384-G/EV+4j2dNv+tEPo3++6LCgdCROaejBqfUeNjuKAiuXbjrxilcCdDz6ZAVfHWe1Y" crossorigin="anonymous"></script>
+</body>
+</html>
