64ce3b449d8dc4f725462f8ec41bc6879c91d042
Francisco Aybar <frannur1.ir@gmail.com>
2025-11-04 12:53:31 +0100

    Sin acabar

---
+++ PHP/relacion3/9relacion3/9formulario.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Relación 3 - Ejercicio 9 - Formulario</title>
+    <link rel="shortcut icon" href="../logo-ies-playamar.png" type="image/x-icon">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+    <style>
+        .form-text 
+        {
+            visibility: hidden;
+        }
+    </style>
+</head>
+<body>
+    <section class="container border border-warning rounded shadow mt-5 p-5 col-4">
+        <form id="form1" action="./9calculo.php" method="post">
+            <h3 class="text-center  text-info-emphasis">Convertidor de texto</h3>
+            
+            <div class="mb-3">
+                <label class="form-label" for="texto">Introduce el texto: <span class="text-danger"> *</span></label>
+                <input class="form-control" type="text" placeholder="Este texto es superguay!" name="texto" id="texto">
+                <div id="textoHelp" class="form-text text-danger">Escribe un texto válido.</div>
+            </div>
+            
+            <input class="form-control btn btn-primary" type="submit" value="Enviar">
+        </form>
+    </section>
+
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.min.js" integrity="sha384-G/EV+4j2dNv+tEPo3++6LCgdCROaejBqfUeNjuKAiuXbjrxilcCdDz6ZAVfHWe1Y" crossorigin="anonymous"></script>
+    <script src="./validaciones.js"></script>
+</body>
+</html>
