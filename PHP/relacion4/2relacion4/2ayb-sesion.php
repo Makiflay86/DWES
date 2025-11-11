23a60301bd298bed67492018b77bef557b0c3645
Francisco Aybar <frannur1.ir@gmail.com>
2025-11-11 20:14:52 +0100

    Terminado

---
+++ PHP/relacion4/2relacion4/2ayb-sesion.php
@@ -0,0 +1,101 @@
+<?php
+    session_start();
+?>
+
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Relación 4 - Ejercicio 1 - Login</title>
+    <link rel="shortcut icon" href="../logo-ies-playamar.png" type="image/x-icon">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+    <style>
+        .form-text 
+        {
+            visibility: hidden;
+        }
+    </style>
+</head>
+<body>
+    <div class="row justify-content-center">
+        <section class="container border border-secondary rounded shadow mt-5 mb-3 p-5 col-12 col-md-8 col-lg-6">
+            <?php 
+                if (!isset($_SESSION["a"]))
+                {
+                    $_SESSION["a"] = 0;
+                }
+
+                if (!isset($_SESSION["b"]))
+                {
+                    $_SESSION["b"] = 0;
+                }
+
+                if (isset($_REQUEST["operacion"]))
+                {
+                    switch($_REQUEST["operacion"])
+                    {
+                        case "+a":
+                            $_SESSION["a"]++;
+                            break;
+
+                        case "-a":
+                            $_SESSION["a"]--;
+                            break;
+
+                        case "+b":
+                            $_SESSION["b"]++;
+                            break;
+
+                        case "-b":
+                            $_SESSION["b"]--;
+                            break;
+                        
+                        case "ra":
+                            $_SESSION["a"] = 0;
+                            break;
+
+                        case "rb":
+                            $_SESSION["b"] = 0;
+                            break;
+
+                        case "ds":
+                            unset($_SESSION["a"]);
+                            unset($_SESSION["b"]);
+                            session_destroy();
+                            header("Location: " . $_SERVER['PHP_SELF']);
+                            break;
+                    }
+                }
+            ?>
+
+            <h1>A: <?php echo $_SESSION["a"]; ?></h1>
+            <h1>B: <?php echo $_SESSION["b"]; ?></h1>
+            
+            <hr>
+    
+            <form id="form1" method="post" action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>">
+                <div class="mb-3">
+                    <select class="form-select" name="operacion" id="operacion">
+                        <option value="+a">Incrementar A</option>
+                        <option value="-a">Decrementar A</option>
+                        <option value="+b">Incrementar B</option>
+                        <option value="-b">Decrementar B</option>
+                        <option value="ra">Resetear A</option>
+                        <option value="rb">Resetear B</option>
+                        <option value="ds">Destruir Sesión</option>
+                    </select>
+                </div>
+                
+                <input type="submit" class="btn btn-primary" name="enviar" value="Enviar">
+            </form>
+
+        </section>
+    </div>
+    
+
+
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.min.js" integrity="sha384-G/EV+4j2dNv+tEPo3++6LCgdCROaejBqfUeNjuKAiuXbjrxilcCdDz6ZAVfHWe1Y" crossorigin="anonymous"></script>
+</body>
+</html>
