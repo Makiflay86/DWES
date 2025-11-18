e3ef012652612f142d9109839c6d404e7a5879cf
Francisco Aybar <frannur1.ir@gmail.com>
2025-11-18 20:00:28 +0100

    trabajando

--- PHP/relacion4/10relacion4/10interfaces.php
+++ PHP/relacion4/10relacion4/10interfaces.php
@@ -1,8 +1,8 @@
 <?php
     interface Encendible
     {
-        function encender();
-        function apagar();
+        public function encender();
+        public function apagar();
     }
 
     class Bombilla implements Encendible /* Implementa la interfaz encendible */
@@ -14,19 +14,71 @@
             private bool $encendida = false
         ) {}
 
-        public function __destruct()
-        {
-            
-        }
+
+
+        public function __destruct() {}
+
+
 
         public function encender()
         {
             $this->encendida = true;
+            echo "Bombilla encendida.";
         }
+
+
 
         public function apagar()
         {
             $this->encendida = false;
+            echo "Bombilla apagada.";
+        }
+        
+    }
+
+
+
+    class Motocicleta implements Encendible /* Implementa la interfaz encendible */
+    {
+        private static int $contadorMatricula = 0;
+
+
+
+        public function  __construct
+        (
+            private int $gasolina = 0,
+            private float $bateria = 2,
+            private bool $matricula = self::$contadorMatricula++,
+            private bool $estado = false
+        ) {}
+
+
+
+        public function __destruct() {}
+
+
+
+        /* Cargar gasolina */
+        public function cargarGasolina($l)
+        {
+            if ($l < 0)
+            {
+                
+            }
+        }
+
+
+
+        public function encender()
+        {
+            $this->estado = true;
+        }
+
+
+
+        public function apagar()
+        {
+            $this->estado = false;
         }
         
     }
