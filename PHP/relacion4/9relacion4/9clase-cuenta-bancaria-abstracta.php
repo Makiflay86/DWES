b34175f038aa1bd6a8d464db5c57d555aa29b9f8
Francisco Aybar <frannur1.ir@gmail.com>
2025-11-21 18:06:29 +0100

    sin acabar

--- PHP/relacion4/9relacion4/9clase-cuenta-bancaria-abstracta.php
+++ PHP/relacion4/9relacion4/9clase-cuenta-bancaria-abstracta.php
@@ -1,5 +1,5 @@
 <?php
-    class CuentaBancaria 
+    abstract class CuentaBancaria
     {
         private $numCuenta; /* El número de la cuenta */
         private $nombre; /* El nombre del titular de la cuenta */
@@ -55,6 +55,11 @@
 
 
         /* SETTERS */
+        public function setSaldo($saldo)
+        {
+            $this->saldo += $saldo;
+        }
+
         public function setNumOperaciones()
         {
             $this->numOperaciones++;
@@ -63,59 +68,13 @@
 
 
         /* Depositar dinero en la cuenta */
-        public function depositarDinero($numCuenta, $cantidad)
-        {
-            if (empty($numCuenta) || $numCuenta == null)
-            {
-                echo "ERROR: Debes de introducir un número de cuenta";
-
-            } else if (empty($cantidad) || $cantidad <= 0)
-            {
-                echo "ERROR: Debes de introducir un cantidad positiva mayor que 0";
-
-            } else 
-            {
-                echo "Operación realizada, dinero depositado.<br>";
-
-                $this->saldo += $cantidad;
-                self::setNumOperaciones();
-            }
-        }
+        public abstract function depositarDinero($numCuenta, $cantidad);
 
 
 
         /* Extraer dinero de la cuenta */
-        public function extraerDinero($numCuenta, $cantidad)
-        {
-            if (empty($numCuenta) || $numCuenta == null)
-            {
-                echo "ERROR: Debes de introducir un número de cuenta";
-
-            } else if (empty($cantidad) || $cantidad <= 0)
-            {
-                echo "ERROR: Debes de introducir un cantidad positiva mayor que 0";
-
-            } else 
-            {
-                $saldo = self::getSaldo();
-                $operacion = $saldo - $cantidad;
-                $mensaje = "";
-
-                if ($operacion < 0)
-                {
-                    $mensaje = "Saldo negativo.";
-
-                } else 
-                {
-                    $mensaje = "Saldo positivo.";
-                }
-                echo "Operación realizada, dinero extraido. - $mensaje <br>";
-                
-                $this->saldo = $operacion;
-                self::setNumOperaciones();
-            }
-        }
-
+        public abstract function extraerDinero($numCuenta, $cantidad);
+        
 
 
         /* toString() */
@@ -155,9 +114,124 @@
     }
 
 
+    class CuentaCredito extends CuentaBancaria
+    {
+        /* Depositar dinero en la cuenta */
+        public function depositarDinero($numCuenta, $cantidad)
+        {
+            if (empty($numCuenta) || $numCuenta == null)
+            {
+                echo "ERROR: Debes de introducir un número de cuenta";
+
+            } else if (empty($cantidad) || $cantidad <= 0)
+            {
+                echo "ERROR: Debes de introducir un cantidad positiva mayor que 0";
+
+            } else 
+            {
+                echo "Operación realizada, dinero depositado.<br>";
+
+                self::setSaldo($cantidad);
+                self::setNumOperaciones();
+            }
+        }
+
+
+
+        /* Extraer dinero de la cuenta */
+        public function extraerDinero($numCuenta, $cantidad)
+        {
+            if (empty($numCuenta) || $numCuenta == null)
+            {
+                echo "ERROR: Debes de introducir un número de cuenta";
+
+            } else if (empty($cantidad) || $cantidad <= 0)
+            {
+                echo "ERROR: Debes de introducir un cantidad positiva mayor que 0";
+
+            } else 
+            {
+                $saldo = self::getSaldo();
+                $operacion = $saldo - $cantidad;
+                $mensaje = "";
+
+                if ($operacion < 0)
+                {
+                    $mensaje = "Saldo negativo.";
+
+                } else 
+                {
+                    $mensaje = "Saldo positivo.";
+                }
+                echo "Operación realizada, dinero extraido. - $mensaje <br>";
+                
+                self::setSaldo(-$cantidad);
+                self::setNumOperaciones();
+            }
+        }
+    }
+
+    class CuentaDebito extends CuentaBancaria
+    {
+        /* Depositar dinero en la cuenta */
+        public function depositarDinero($numCuenta, $cantidad)
+        {
+            if (empty($numCuenta) || $numCuenta == null)
+            {
+                echo "ERROR: Debes de introducir un número de cuenta";
+
+            } else if (empty($cantidad) || $cantidad <= 0)
+            {
+                echo "ERROR: Debes de introducir un cantidad positiva mayor que 0";
+
+            } else 
+            {
+                echo "Operación realizada, dinero depositado.<br>";
+
+                self::setSaldo($cantidad);
+                self::setNumOperaciones();
+            }
+        }
+
+
+
+        /* Extraer dinero de la cuenta */
+        public function extraerDinero($numCuenta, $cantidad)
+        {
+            if (empty($numCuenta) || $numCuenta == null)
+            {
+                echo "ERROR: Debes de introducir un número de cuenta";
+
+            } else if (empty($cantidad) || $cantidad <= 0)
+            {
+                echo "ERROR: Debes de introducir un cantidad positiva mayor que 0";
+
+            } else 
+            {
+                $saldo = self::getSaldo();
+                $operacion = $saldo - $cantidad;
+                $mensaje = "";
+
+                if ($operacion < 0)
+                {
+                    $mensaje = "Saldo negativo.";
+
+                } else 
+                {
+                    $mensaje = "Saldo positivo.";
+                }
+                echo "Operación realizada, dinero extraido. - $mensaje <br>";
+                
+                self::setSaldo($operacion);
+                self::setNumOperaciones();
+            }
+        }
+    }
+
+
 
 echo "<h3>Creando una cuenta bancaria</h3>";
-$cb1 = new CuentaBancaria(1234, "Francisco S.L", 1500);
+$cb1 = new CuentaCredito(1234, "Francisco S.L", 1500);
 echo $cb1;
 
 
@@ -190,10 +264,11 @@ echo "<br>============================================<br>";
 
 
 echo "<h3>Transferir dinero a otra cuenta</h3>";
-$cb2 = new CuentaBancaria(33, "Anonimous S.A", 33500);
+$cb2 = new CuentaDebito(33, "Anonimous S.A", 33500);
 echo "$cb1<br>";
-echo $cb2;
+echo "$cb2<br>";
 
+echo "<br>Dinero a transferir: 501€<br>";
 CuentaBancaria::transferirDinero($cb1, $cb2, 501);
 
 echo "<br>$cb1<br>";
