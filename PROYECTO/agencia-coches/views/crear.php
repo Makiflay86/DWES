1b9930810a78e6f4bb1b398ae310a18cdf7ef923
Francisco Aybar <frannur1.ir@gmail.com>
2026-01-13 21:18:18 +0100

    quitar la decoracion del enlace

--- PROYECTO/agencia-coches/views/crear.php
+++ PROYECTO/agencia-coches/views/crear.php
@@ -5,21 +5,53 @@
 <head>
     <meta charset="UTF-8">
     <title>Crear Coche</title>
+
+    <!-- Bootstrap -->
+    <link
+        href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css"
+        rel="stylesheet"
+        integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN"
+        crossorigin="anonymous"
+    />
+
+    <link rel="stylesheet" href="./views/src/css/style.css">
 </head>
 
 <body>
-    <h2>Crear Nuevo Coche</h2>
-    <form method="POST" action="index.php?action=create">
-        <label>Marca: <input type="text" name="marca" required></label><br>
-        <label>Modelo: <input type="text" name="modelo" required></label><br>
-        <label>Fecha de Fabricación: <input type="date" name="fechaFabricacion" required></label><br>
-        <label>Kilometros: <input type="number" name="kilometros" required></label><br>
-        <label>Combustible: <input type="text" name="combustible" required></label><br>
-        <label>Color: <input type="text" name="color" required></label><br>
-        <label>Imágen: <input type="file" name="imagen" accept="image/*" required></label><br>
-        <button type="submit">Crear Coche</button>
-    </form>
-    <p><a href="index.php?action=index">Volver al listado</a></p>
+
+    <div class="container-fluid w-50 mx-auto">
+
+
+        <h2>Crear Nuevo Coche</h2>
+        <form method="POST" action="index.php?action=create">
+            <label>Marca: <input type="text" name="marca" required></label><br>
+            <label>Modelo: <input type="text" name="modelo" required></label><br>
+            <label>Fecha de Fabricación: <input type="date" name="fechaFabricacion" required></label><br>
+            <label>Kilometros: <input type="number" name="kilometros" required></label><br>
+            <label>Combustible: <input type="text" name="combustible" required></label><br>
+            <label>Color: <input type="text" name="color" required></label><br>
+            <label>Imágen: <input type="file" name="imagen" accept="image/*" required></label><br>
+            <button type="submit">Crear Coche</button>
+        </form>
+        <p><a href="index.php?action=index">Volver al listado</a></p>
+
+
+    </div>
+
+
+
+    <!-- Bootstrap Scripts -->
+     <script
+        src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js"
+        integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r"
+        crossorigin="anonymous"
+    ></script>
+
+    <script
+        src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js"
+        integrity="sha384-BBtl+eGJRgqQAUMxJ7pMwbEyER4l1g+O15P+16Ep7Q9Q+zqX6gSbd85u4mG4QzX+"
+        crossorigin="anonymous"
+    ></script>
 </body>
 
 </html>
--- PROYECTO/agencia-coches/views/listar.php
+++ PROYECTO/agencia-coches/views/listar.php
@@ -67,8 +67,8 @@
                         <svg class="bi bi-plus-circle-fill" xmlns="http://www.w3.org/2000/svg" width="30" fill="white" viewBox="0 0 16 16">
                             <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0M8.5 4.5a.5.5 0 0 0-1 0v3h-3a.5.5 0 0 0 0 1h3v3a.5.5 0 0 0 1 0v-3h3a.5.5 0 0 0 0-1h-3z"/>
                         </svg>
+                        <span class="ps-1 text-black">Añadir coche</span>
                     </a>
-                    <span class="ps-1">Añadir coche</span>
                 </button>
             </div>
         </div>
