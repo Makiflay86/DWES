75a2616f96f50cac48ef9f9e1617401111343795
Francisco Aybar <frannur1.ir@gmail.com>
2026-01-16 10:23:43 +0100

    Arreglando problema de scroll

--- PROYECTO/agencia-coches/views/src/js/nav-scroll.js
+++ PROYECTO/agencia-coches/views/src/js/nav-scroll.js
@@ -1,26 +1,33 @@
 let lastScrollTop = 0;
 const navbar = document.getElementById("mainNavbar");
 
-window.addEventListener("scroll", function() {
+window.addEventListener("scroll", function() 
+{
     let scrollTop = window.pageYOffset || document.documentElement.scrollTop;
     
     // NUEVO: Si la página es bajita (como el formulario), no hagas nada
-    // Solo actúa si el contenido es al menos 600px más grande que la pantalla
-    if (document.documentElement.scrollHeight < window.innerHeight + 600) {
+    // Solo actúa si el contenido es al menos 300px más grande que la pantalla
+    if (document.documentElement.scrollHeight < window.innerHeight + 300) 
+    {
         navbar.classList.remove("nav-fixed-up", "nav-hidden");
         return; 
     }
 
-    if (scrollTop <= 0) {
+    if (scrollTop <= 0) 
+    {
         navbar.classList.remove("nav-fixed-up", "nav-hidden");
         return;
     }
 
-    if (scrollTop > 50) {
-        if (scrollTop < lastScrollTop) {
+    if (scrollTop > 50) 
+    {
+        if (scrollTop < lastScrollTop) 
+        {
             navbar.classList.add("nav-fixed-up");
             navbar.classList.remove("nav-hidden");
-        } else {
+
+        } else 
+        {
             navbar.classList.add("nav-hidden");
             navbar.classList.remove("nav-fixed-up");
         }
