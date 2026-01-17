9381007c66c466b73c1e846032ba2bd0c6c106aa
Francisco Aybar <frannur1.ir@gmail.com>
2026-01-17 19:40:39 +0100

    actualización de la base de datos

--- PROYECTO/agencia-coches/bd-agencia-coches.sql
+++ PROYECTO/agencia-coches/bd-agencia-coches.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 17-01-2026 a las 15:41:39
+-- Tiempo de generación: 17-01-2026 a las 19:40:14
 -- Versión del servidor: 10.4.32-MariaDB
 -- Versión de PHP: 8.2.12
 
