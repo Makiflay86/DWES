cd0531b4af5c25e7c2df008dc79f4d8489ac6a58
Francisco Aybar <frannur1.ir@gmail.com>
2026-01-20 20:46:30 +0100

    añadido los botones de accion

--- LARAVEL/proyecto-uno/resources/views/layouts/app.blade.php
+++ LARAVEL/proyecto-uno/resources/views/layouts/app.blade.php
@@ -16,6 +16,9 @@
             integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN"
             crossorigin="anonymous"
         />
+
+        <!-- Bootstrap Icon v1.13.1 -->
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.13.1/font/bootstrap-icons.min.css">
     </head>
 
     <body class="pt-5">
--- LARAVEL/proyecto-uno/resources/views/productos/index.blade.php
+++ LARAVEL/proyecto-uno/resources/views/productos/index.blade.php
@@ -6,15 +6,16 @@
 
 @section('content')
     <div class="container-flud w-50 mx-auto my-5">
-        <div class="text-center mb-3">
-            <h1>Soy un producto → 🧅</h1>
+        <div class="text-center mb-5">
+            <h1>Listado de los productos → 🧅</h1>
         </div>
-        <table class="table table-bordered shadow-sm">
+        <table class="table table-bordered shadow-sm text-center align-middle">
             <tr class="table-dark">
                 <th>ID</th>
                 <th>CÓDIGO</th>
                 <th>DESCRIPCIÓN</th>
                 <th>PRECIO</th>
+                <th>ACCIÓN</th>
             </tr>
             @foreach ($productos as $producto)
             <tr>
@@ -22,6 +23,14 @@
                 <td>{{$producto->codigo}}</td>
                 <td>{{$producto->descripcion}}</td>
                 <td>{{$producto->precio}} €</td>
+                <td>
+                    <a href="" class="btn btn-warning">
+                        <i class="bi bi-pencil-square"></i>
+                    </a>
+                    <a href="" class="btn btn-danger">
+                        <i class="bi bi-x-square"></i>
+                    </a>
+                </td>
             </tr>
             @endforeach
         </table>
