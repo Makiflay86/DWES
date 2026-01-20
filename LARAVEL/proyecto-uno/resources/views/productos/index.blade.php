003ab27bbe6db68445ba63b8098275af80b8cc42
Francisco Aybar <frannur1.ir@gmail.com>
2026-01-20 20:34:14 +0100

    mostrando la lista de productos en la vista

--- LARAVEL/proyecto-uno/resources/views/productos/index.blade.php
+++ LARAVEL/proyecto-uno/resources/views/productos/index.blade.php
@@ -5,7 +5,25 @@
 @endsection
 
 @section('content')
-    <div class="container-flud w-50 mx-auto my-5 text-center">
-        <h1>Soy un producto → 🧅</h1>
+    <div class="container-flud w-50 mx-auto my-5">
+        <div class="text-center mb-3">
+            <h1>Soy un producto → 🧅</h1>
+        </div>
+        <table class="table table-bordered shadow-sm">
+            <tr class="table-dark">
+                <th>ID</th>
+                <th>CÓDIGO</th>
+                <th>DESCRIPCIÓN</th>
+                <th>PRECIO</th>
+            </tr>
+            @foreach ($productos as $producto)
+            <tr>
+                <td>{{$producto->id}}</td>
+                <td>{{$producto->codigo}}</td>
+                <td>{{$producto->descripcion}}</td>
+                <td>{{$producto->precio}} €</td>
+            </tr>
+            @endforeach
+        </table>
     </div>
 @endsection
