a66aceda490c21baadc306c80f7ef2ceee5d34b3
Francisco Aybar <frannur1.ir@gmail.com>
2026-01-27 19:32:21 +0100

    step 9

--- LARAVEL/api-restful/app/Http/Controllers/AuthController.php
+++ LARAVEL/api-restful/app/Http/Controllers/AuthController.php
@@ -3,7 +3,10 @@
 namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\ValidationException;
 
 class AuthController extends Controller
 {
