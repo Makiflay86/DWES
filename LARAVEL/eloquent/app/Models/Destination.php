8bd19ec080d316431d8915ae3d26b9176d0c22bb
Francisco Aybar <frannur1.ir@gmail.com>
2026-02-13 16:37:54 +0100

    empezando con eloquent

---
+++ LARAVEL/eloquent/app/Models/Destination.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Destination extends Model
+{
+    /* const CREATED_AT = "creation_date";
+    const UPDATED_AT = "updated_date";
+
+    protected $table = "list_destination";
+
+    protected $primaryKey = "identificador";
+
+    public $incrementing = false;
+
+    protected $keyType = 'string';
+
+    protected $dateFormat = "U"; */
+}
